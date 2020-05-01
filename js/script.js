0ae793dfe0b074bc54ee8fbab65d38487bbce2a7
Leonid Panasiuk <51416748+Leonid-Panasiuk@users.noreply.github.com>
2020-05-01 22:10:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-//ANIMATIONS
-
-AOS.init({
-  easing: 'ease-in-out-sine',
-  once:true
-});
-
-setInterval(addItem, 300);
-
-var itemsCounter = 1;
-var container = document.getElementById('aos-demo');
-
-function addItem() {
-  if (itemsCounter > 42) return;
-  var item = document.createElement('div');
-  item.classList.add('aos-item');
-  item.setAttribute('data-aos', 'fade-up');
-  item.innerHTML = '<div class="aos-item__inner"><h3>' + itemsCounter + '</h3></div>';
-  itemsCounter++;
-}
 
 //MAP
 
